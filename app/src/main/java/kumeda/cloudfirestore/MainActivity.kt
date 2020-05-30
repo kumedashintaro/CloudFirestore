36ec03689fea44fe9baf2302deb3197842fac02f
shintaro kumeda <pii.k2.bys@gmail.com>
2020-05-30 22:47:50 +0900

    Initial commit

---
+++ app/src/main/java/kumeda/cloudfirestore/MainActivity.kt
@@ -0,0 +1,17 @@
+package kumeda.cloudfirestore
+
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import java.sql.Date
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
