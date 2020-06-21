aa7279f6e27d1953f668fd07c8ec287548f200e9
shintaro kumeda <pii.k2.bys@gmail.com>
2020-06-21 17:16:20 +0900

    username post 削除

--- app/src/main/java/kumeda/cloudfirestore/Activities/AddThoughtActivity.kt
+++ app/src/main/java/kumeda/cloudfirestore/Activities/AddThoughtActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FieldValue
 import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.android.synthetic.main.activity_add_thought.*
@@ -28,7 +29,7 @@ class AddThoughtActivity : AppCompatActivity() {
         data.put(NUM_LIKES, 0)
         data.put(THOUGHT_TXT, addThoughtTxt.text.toString())
         data.put(TIMESTAMP, FieldValue.serverTimestamp())
-        data.put(USERNAME, addUsernameTxt.text.toString())
+        data.put(USERNAME, FirebaseAuth.getInstance().currentUser?.displayName.toString())
 
 
         FirebaseFirestore.getInstance().collection(THOUGHTS_REF)
